9f5a45ee480e110add19ed3b6f470d69b4b1e516
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-05-21 20:41:37 +0300

    JavaScript Promises Async Await and Ajax

--- script.js
+++ script.js
@@ -4,23 +4,28 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 const renderCountry = function(data, className = '') {
+    const countryName = data.name.common || '';
+    const flagUrl = data.flags.png || data.flags.svg || '';
     const html = `
     <article class="country ${className}">
-      <img class="country__img" src="${data.flags.png}" />
+      <img class="country__img" src="${flagUrl}" />
       <div class="country__data">
-        <h3 class="country__name">${data.name}</h3>
+        <h3 class="country__name">${countryName}</h3>
         <h4 class="country__region">${data.region}</h4>
         <p class="country__row"><span>👫</span>${(
           +data.population / 1000000
         ).toFixed(1)} people</p>
-        <p class="country__row"><span>🗣️</span>${data.languages[0]}</p>
-        <p class="country__row"><span>💰</span>${data.currencies[0]}</p>
+        <p class="country__row"><span>🗣️</span>${data.languages.heb}</p>
+        <p class="country__row"><span>💰</span>${data.currencies.ILS.name}</p>
       </div>
     </article>
-    `;
+  `;
     countriesContainer.insertAdjacentHTML('beforeend', html);
     countriesContainer.style.opacity = 1;
 };
+
+
+
 
 const renderError = function(msg) {
     countriesContainer.insertAdjacentText('beforeend', msg);
@@ -322,8 +327,8 @@ Promise.resolve('abc').then(x => console.log(x))
 Promise.reject(new Error('Problem!')).catch(x => console.error(x))
 */
 
+/*
 //* Promisifying the Geolocation API
-
 const getPosition = function() {
     return new Promise(function(resolve, reject) {
         // navigator.geolocation.getCurrentPosition(
@@ -362,3 +367,31 @@ const whereAmI = function() {
 };
 
 btn.addEventListener('click', whereAmI)
+*/
+
+//* Consuming Promises with Async/Await
+const getPosition = function() {
+    return new Promise(function(resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+}
+
+const whereAmI = async function() {
+    // Geolocation
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
+
+    // Reverse geocoding
+    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    const dataGeo = await resGeo.json()
+    console.log(dataGeo)
+
+    // Country data
+    const res = await fetch(`https://restcountries.com/v3.1/name/${dataGeo.country}`);
+    const data = await res.json();
+    console.log(data[0]);
+    renderCountry(data[0]);
+}
+
+whereAmI();
+console.log('FIRST');
