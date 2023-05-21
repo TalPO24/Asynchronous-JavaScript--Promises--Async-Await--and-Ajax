c8e1956a2906fdf0777325eb8f2e910f657c99bf
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-05-21 13:48:44 +0300

    JavaScript Promises Async Await and Ajax

--- script.js
+++ script.js
@@ -219,6 +219,7 @@ btn.addEventListener('click', function() {
 getCountryData('australia');
 */
 
+/*
 //* Coding Challenge #1
 const whereAmI = function(lat, lng) {
     fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
@@ -243,3 +244,17 @@ const whereAmI = function(lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+
+*/
+
+//* The Event Loop
+console.log('Test start')
+setTimeout(() => console.log('0 sec timer'), 0)
+Promise.resolve('resolved promise 1').then(res => console.log(res))
+
+Promise.resolve('resolved promise 2').then(res => {
+    for (let i = 0; i < 1000000000; i++) {}
+    console.log(res)
+})
+
+console.log('Test end')
